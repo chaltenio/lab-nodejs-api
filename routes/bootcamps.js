2cb7f9a2d98d186b1671135f1374862d5539d3b0
chaltenio <dev@chalten.io>
2020-04-21 23:49:57 +1200

    Just tidying up the code

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-  });
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
   
 router.get('/:id', (req, res) => {
 res
@@ -12,17 +12,17 @@ res
 });
 
 router.post('/', (req, res) => {
-res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
 });
 
 router.put('/:id', (req, res) => {
-res
+  res
     .status(200)
     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
 router.delete('/:id', (req, res) => {
-res
+  res
     .status(200)
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
