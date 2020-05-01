2557e1a4b328843b17ec50ce8d42a5b9c3c721d1
chaltenio <dev@chalten.io>
2020-05-02 11:32:38 +1200

    Implementing registration + bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
-const Bootcamp = require('../models/Bootcamp');
+const User = require('../models/User');
 
 
 // @desc    Register user
@@ -8,5 +8,15 @@ const Bootcamp = require('../models/Bootcamp');
 // @access  Public
 
 exports.register = asyncHandler( async (req, res, next) => {
+    const { name, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
     res.status(200).json({ success: true });
 });
