ecb563b890c0978dcf1f0e20ca71db4e546361f6
chaltenio <dev@chalten.io>
2020-05-02 17:36:53 +1200

    Role Authorization

--- routes/courses.js
+++ routes/courses.js
@@ -13,7 +13,7 @@ const advanceResults = require('../middleware/advanceResults');
 
 const router = express.Router({ mergeParams: true });
 
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 router
   .route('/')
@@ -24,8 +24,8 @@ router
     }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
   
-router.route('/:id').get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router.route('/:id').get(getCourse).put(protect, authorize('publisher', 'admin'), updateCourse).delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
