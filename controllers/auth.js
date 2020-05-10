773694a016a0ecd98c2655594e538fb062083ef5
chaltenio <dev@chalten.io>
2020-05-10 15:20:21 +1200

    Forgot Password - Generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,30 +47,6 @@ exports.login = asyncHandler( async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
-// Get token from model, create cookie and send reposnse
-const sendTokenResponse = (user, statusCode, res) => {
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-
-  if(process.env.NOOD_ENV === 'production') {
-      options.secure = true;
-  }
-
-  //console.log(options.expires);
-
-  res
-    .status(statusCode)
-    .cookie('token', token, options)
-    .json({ success: true, token });
-};
-
 
 // @desc    Get current logged in user
 // @route   POST /api/v1/auth/me
@@ -83,3 +59,51 @@ exports.getMe = asyncHandler(async (req, res, next) => {
         data: user
     });
 });
+
+
+// @desc    Forgot password
+// @route   POST /api/v1/auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user =  await User.findOne({ email: req.body.email });
+
+    if(!user){
+        return next(new ErrorResponse('There is no user with that email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false});
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+
+// Get token from model, create cookie and send reposnse
+const sendTokenResponse = (user, statusCode, res) => {
+    // Create token
+    const token = user.getSignedJwtToken();
+  
+    const options = {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    };
+  
+    if(process.env.NOOD_ENV === 'production') {
+        options.secure = true;
+    }
+  
+    //console.log(options.expires);
+  
+    res
+      .status(statusCode)
+      .cookie('token', token, options)
+      .json({ success: true, token });
+  };
+
